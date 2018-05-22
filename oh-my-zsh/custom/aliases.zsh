0749fd6529204eb12dc62f4da6ec4e90ce3a8ded
acarta <adc1009@wildcats.unh.edu>
2018-05-22 02:16:15 -0400

    fixed problem with zsh custom dir not committing properly

---
+++ oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,4 @@
+alias -g vim="$(which nvim)"
+alias -g vi=vim
+alias -g emacs="$(which emacs)"
+alias -g firefox="/opt/mozilla/firefox/firefox"
---
+++ oh-my-zsh/custom/env.zsh
@@ -0,0 +1,3 @@
+export EDITOR="$(which nvim)"
+export LD_LIBRARY_PATH="${HOME}/channelflow_parallel/lib:${LD_LIBRARY_PATH}"
+export PATH="${HOME}/channelflow_parallel/bin:${PATH}"
