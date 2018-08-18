af13f8404399ec6cbf2ac93e81d3180893f0d34a
Aaron D. Carta <acarta@janeway>
2018-08-18 19:42:51 -0400

    Added aptitude aliases

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -1,4 +1,29 @@
-alias -g vim="$(which nvim)"
-alias -g vi=vim
-alias -g emacs="$(which emacs)"
-alias -g firefox="/opt/mozilla/firefox/firefox"
+#!/usr/bin/zsh
+
+# set emacs aliases
+function _set_emacs_aliases()
+{
+	  if [ -x "$(which emacs)" ]; then
+		    alias ed="emacs --daemon"
+		    alias ec="emacsclient -nc"
+	  fi
+}
+
+# set apt aliases
+function _set_pkg_aliases()
+{
+	  if [ -x $(which apt) ]; then
+		    alias pkgsearch='aptitude search'
+		    alias pkgshow='aptitude show'
+		    alias pkginst='sudo aptitude install -V'
+		    alias pkgupd='sudo aptitude update'
+		    alias pkgupg='sudo aptitude dist-upgrade -V && sudo aptitude autoremove'
+		    alias pkgupgd='sudo aptitude update && sudo aptitude dist-upgrade -V && sudo aptitude autoremove'
+		    alias pkgrm='sudo aptitude remove'
+		    alias pkgpurge='sudo aptitude remove --purge'
+		    alias pkgfiles='dpkg --listfiles'
+	  fi
+}
+
+_set_emacs_aliases
+_set_pkg_aliases
