1be9a494cfa096eff9d2564e11c553aafa4afb21
Aaron D. Carta <adc1009@wildcats.unh.edu>
2019-03-29 12:18:42 -0400

    added some more aliases
    Signed-off-by: Aaron D. Carta <adc1009@wildcats.unh.edu>

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -3,30 +3,31 @@
 # set emacs aliases
 function _set_emacs_aliases()
 {
-	  if [ -x "$(which emacs)" ]; then
-		    alias ed="emacs --daemon"
-		    alias ec="emacsclient -nc"
+    if [ -x "$(which emacs)" ]; then
+        alias ed="emacs --daemon"
+        alias ec="emacsclient -nc"
+        alias edc="ed && ec"
+    fi
+}
+
+#set apt aliases
+function _set_pkg_aliases()
+{
+	  if [ -x $(which apt) ]; then
+		    alias pkgsearch='aptitude search'
+		    alias pkgshow='apt show'
+		    alias pkginst='sudo apt install -V'
+		    alias pkgupd='sudo apt update'
+		    alias pkgupg='sudo apt dist-upgrade -V && sudo apt autoremove'
+		    alias pkgupgd='sudo apt update && sudo apt dist-upgrade -V && sudo apt autoremove'
+		    alias pkgrm='sudo apt remove'
+		    alias pkgpurge='sudo apt remove --purge'
+		    alias pkgfiles='dpkg --listfiles'
 	  fi
 }
 
-# set apt aliases
-#function _set_pkg_aliases()
-#{
-#	  if [ -x $(which apt) ]; then
-#		    alias pkgsearch='aptitude search'
-#		    alias pkgshow='apt show'
-#		    alias pkginst='sudo apt install -V'
-#		    alias pkgupd='sudo apt update'
-#		    alias pkgupg='sudo apt dist-upgrade -V && sudo apt autoremove'
-#		    alias pkgupgd='sudo apt update && sudo apt dist-upgrade -V && sudo apt autoremove'
-#		    alias pkgrm='sudo apt remove'
-#		    alias pkgpurge='sudo apt remove --purge'
-#		    alias pkgfiles='dpkg --listfiles'
-#	  fi
-#}
-
 _set_emacs_aliases
-#_set_pkg_aliases
+_set_pkg_aliases
 
 alias firefox="/opt/firefox/firefox"
 alias ipythonqt="ipython --pylab=qt"
