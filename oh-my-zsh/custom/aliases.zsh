19440ed8f2d8e4a0ebe2df2498852903b193ccac
Aaron D. Carta <acarta@picard>
2020-01-28 19:00:58 -0500

    removed unused aliases and made alias for julia consistent
    Signed-off-by: Aaron D. Carta <acarta@picard>

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -27,10 +27,6 @@ function _set_pkg_aliases()
 _set_emacs_aliases
 _set_pkg_aliases
 
-# alias firefox="/opt/firefox/firefox"
 alias ipythonqt="ipython --pylab=qt"
 alias tailnf="tail -n20 -f"
-# alias julia="${HOME}/julia/julia-1.1.1/julia"
-# alias julia="${HOME}/julia/julia-1.1.1/julia --banner=no -O2 --track-allocation --compile=yes"
-alias srx="source ${HOME}/.xinitrc"
-alias ub="${HOME}/.config/i3/update_background.sh"
+alias julia="${HOME}/julia/julia-1.0.5/bin/julia"
